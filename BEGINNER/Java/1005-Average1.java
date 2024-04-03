b694637b3fbb891727dd94685e924278008414b1
Vitoriapm <155494901+Vitoriapm@users.noreply.github.com>
2024-04-03 11:46:28 -0300

    Create 1005-Average1.java

---
+++ BEGINNER/Java/1005-Average1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double MEDIA = ((A*3.5) + (B*7.5))/11;
+        System.out.println("MEDIA = " + String.format("%.5f",MEDIA));
+    }
+}
