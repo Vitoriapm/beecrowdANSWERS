84751572ea9cdf650c16e956689e8745d88325e4
Vitória P.M. <155494901+Vitoriapm@users.noreply.github.com>
2024-11-26 14:42:13 -0300

    Create  1006-Average2.php
    1006 beecrowd

---
+++ BEGINNER/PHP/1006-Average2.php
@@ -0,0 +1,7 @@
+<?php
+    $a = (float)readLine() * 2;
+    $b = (float)readLine() * 3;
+    $c = (float)readLine() * 5;
+    $result = ($a + $b + $c)/10;
+    echo "MEDIA = " . number_format($result,1,".","") . "\n";
+?>
