6a0cde866775ad14244846e041006160f4a98ee8
Vitória P.M. <155494901+Vitoriapm@users.noreply.github.com>
2024-11-26 14:50:20 -0300

    Create 1007-Difference.php
    1007 beecrowd

---
+++ BEGINNER/PHP/1007-Difference.php
@@ -0,0 +1,8 @@
+<?php
+    $a = (int)readLine();
+    $b = (int)readLine();
+    $c = (int)readLine();
+    $d = (int)readLine();
+    $prod = ($a * $b - $c * $d);
+    echo "DIFERENCA = $prod\n";
+?>
