cf468a4d9428cdfe2e7c1ff18dc4d486590c87e7
Vitória P.M. <155494901+Vitoriapm@users.noreply.github.com>
2024-11-28 16:04:26 -0300

    Update 1038-Snack.php
    1038 beecrowd

--- BEGINNER/PHP/1038-Snack.php
+++ BEGINNER/PHP/1038-Snack.php
@@ -1,12 +1,12 @@
 <?php
-    $itens = array(1 => 4, 2 => 4.5, 3 => 5, 4 => 2, 5 => 1.5);
+$itens = array(1 => 4, 2 => 4.5, 3 => 5, 4 => 2, 5 => 1.5);
     
-    $x = explode(" ", fgets(STDIN));
+$x = explode(" ", fgets(STDIN));
     
-    foreach($itens as $code => $price){
-        if($x[0] == $code){
-            $total = $x[1] * $price;
-            echo "Total: R$ " . number_format($total, 2) . "\n";
-        }
+foreach($itens as $code => $price){
+    if($x[0] == $code){
+        $total = $x[1] * $price;
+        echo "Total: R$ " . number_format($total, 2) . "\n";
     }
+}
 ?>
