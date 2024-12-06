8d7e0680d04371a54c2ac5fd44214337d370352a
Vitória P.M. <155494901+Vitoriapm@users.noreply.github.com>
2024-12-05 22:46:37 -0300

    Create 1052-Month.php
    beecrowd 1052

---
+++ BEGINNER/PHP/1052-Month.php
@@ -0,0 +1,14 @@
+<?php
+
+$userNum = readLine();
+
+$months = array(1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 
+                7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "December");
+
+foreach ($months as $number => $name){
+    if($month == $number){
+        echo "$name\n";
+    }
+}
+
+?>
