4b68f794e1c87d8301c66d46a3c3e32f6db15fb9
Vitória P.M. <155494901+Vitoriapm@users.noreply.github.com>
2026-07-22 22:36:36 -0300

    Exercício 1014

--- BEGINNER/Java/1014-Consumption.java
+++ BEGINNER/Java/1014-Consumption.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 		
-    		int x = sc.nextInt();
-    		double y = sc.nextDouble();
+    	int x = sc.nextInt();
+    	double y = sc.nextDouble();
     		
-    		System.out.printf("%.3f km/l%n", x / y);
+    	System.out.printf("%.3f km/l%n", x / y);
      
         sc.close();
     }
