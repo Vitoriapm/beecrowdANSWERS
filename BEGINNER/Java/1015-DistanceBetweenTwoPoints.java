09f63f903dbfe4ebed28449fa6bd5774c055195d
Vitória P.M. <155494901+Vitoriapm@users.noreply.github.com>
2026-07-23 22:49:49 -0300

    Exercicio 1015

---
+++ BEGINNER/Java/1015-DistanceBetweenTwoPoints.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+      Scanner sc = new Scanner(System.in);
+		  double x1, y1, x2, y2;
+		
+  		x1 = sc.nextDouble();	
+  		y1 = sc.nextDouble();	
+  		x2 = sc.nextDouble();	
+  		y2 = sc.nextDouble();	
+  		
+  		double distance = Math.sqrt(
+  			    Math.pow(x2 - x1, 2) +
+  			    Math.pow(y2 - y1, 2)
+			);
+
+			System.out.printf("%.4f%n", distance);
+			
+			sc.close();
+    }
+
+}
